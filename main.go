45ca67b9312454c13f6d0e81838b45ad1d9f7c55
A. Schulze <git@andeasschulze.de>
2023-10-22 21:22:01 +0200

    disable deprecated tls versions

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"golang.org/x/crypto/acme/autocert"
 
 	"context"
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -159,6 +160,7 @@ func main() {
 		}
 		srv.Addr = ":https"
 		srv.TLSConfig = cm.TLSConfig()
+		srv.TLSConfig.MinVersion = tls.VersionTLS12
 	}
 	port := os.Getenv("PORT")
 	if port == "" {
